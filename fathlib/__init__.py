d76c981ac25b1cdcd0ee464e4bb9200c9b81d292
Noah Kim <noahbkim@gmail.com>
2026-01-18 19:14:12 -0500

    Fix Python

--- fathlib/__init__.py
+++ fathlib/__init__.py
@@ -6,15 +6,21 @@ import posixpath
 
 from .core import PosixFath as PosixFath
 from .core import WindowsFath as WindowsFath
-from .core import posix_normalize_dot as posix_normalize_dot
 from .core import posix_normalize_slash as posix_normalize_slash
+from .core import posix_normalize_dot as posix_normalize_dot
 from .core import posix_normalize as posix_normalize
 from .core import posix_join as posix_join
-from .core import windows_normalize_dot as windows_normalize_dot
+from .core import posix_root as posix_root
+from .core import posix_name as posix_name
+from .core import posix_parent as posix_parent
 from .core import windows_normalize_slash as windows_normalize_slash
+from .core import windows_normalize_dot as windows_normalize_dot
 from .core import windows_normalize as windows_normalize
 from .core import windows_join as windows_join
 from .core import windows_drive as windows_drive
+from .core import windows_root as windows_root
+from .core import windows_name as windows_name
+from .core import windows_parent as windows_parent
 
 
 class PurePosixPath(PosixFath):
