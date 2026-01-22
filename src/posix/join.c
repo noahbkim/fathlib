3b413599d65e262c4128b2156c1ceaa88c923830
Noah Kim <noahbkim@gmail.com>
2026-01-21 20:47:03 -0500

    WIP

--- src/posix/join.c
+++ src/posix/join.c
@@ -7,50 +7,112 @@
 // MARK: Join
 
 PyUnicodeObject *
-_posix_join(PyObject **args, Py_ssize_t nargs)
+_posix_join_zero()
 {
-    if (nargs == 0)
+    return (PyUnicodeObject *)PyUnicode_FromString(".");
+}
+
+PyUnicodeObject *
+_posix_join_one(PyObject *arg)
+{
+    PyUnicodeObject *fspath = _fspath(arg);
+    if (!fspath)
     {
-        return (PyUnicodeObject *)PyUnicode_FromString(".");
+        return NULL;
     }
-    else if (nargs == 1)
+    PyUnicodeObject *normalized = _posix_normalize(fspath);
+    Py_DECREF(fspath);
+    return normalized;
+}
+
+PyUnicodeObject *
+_posix_join_many(PyObject **args, Py_ssize_t nargs)
+{
+    PyUnicodeObject *joined = NULL;
+    PyUnicodeObject **fspaths = calloc(nargs, sizeof(PyUnicodeObject *));
+    Py_ssize_t i = nargs;
+    do
     {
-        PyUnicodeObject *fspath = _fspath(args[0]);
+        i -= 1;
+        PyUnicodeObject *fspath = _fspath((PyObject *)args[i]);
         if (!fspath)
         {
-            return NULL;
+            goto error;
         }
-        PyUnicodeObject *normalized = _posix_normalize(fspath);
-        Py_DECREF(fspath);
-        return normalized;
-    }
-    else
-    {
-        PyUnicodeObject *joined = NULL;
-        PyUnicodeObject **fspaths = malloc(sizeof(PyUnicodeObject *) * nargs);
-        Py_ssize_t i = nargs;
-        do
+        fspaths[i] = fspath;
+        if (_posix_is_absolute(fspath))
         {
-            i -= 1;
-            PyUnicodeObject *fspath = _fspath((PyObject *)args[i]);
-            if (!fspath)
-            {
-                goto error;
-            }
-            fspaths[i] = fspath;
-            if (_posix_is_absolute(fspath))
-            {
-                break;
-            }
+            break;
         }
-        while (i > 0);
+    }
+    while (i > 0);
 
-        joined = _join('/', fspaths + i, nargs - i);
+    // Last element is absolute, do no work
+    if (i == nargs - 1)
+    {
+        joined = _posix_normalize(fspaths[0]);
+        goto error; // think of this as goto done
+    }
+
+    Py_ssize_t write_size = PyUnicode_GET_LENGTH(fspaths[i]);
+    unsigned int write_maxchar = PyUnicode_MAX_CHAR_VALUE(fspaths[i]);
+    for (Py_ssize_t j = i + 1; j < nargs; ++j)
+    {
+        write_size += 1;
+        write_size += PyUnicode_GET_LENGTH(fspaths[j]);
+        unsigned int arg_maxchar = PyUnicode_MAX_CHAR_VALUE(fspaths[j]);
+        write_maxchar = Py_MAX(write_maxchar, arg_maxchar);
+    }
+
+    PyUnicodeObject *write = (PyUnicodeObject *)PyUnicode_New(write_size, write_maxchar);
+    if (!write)
+    {
+        return NULL;
+    }
+
+    Cow cow;
+    cow_construct_writer(&cow, write);
+    PosixNormalize state;
+    if (_posix_normalize_impl(&cow, &state, fspaths[i]) != 0)
+    {
+        goto error;
+    }
+    for (Py_ssize_t j = i + 1; j < nargs; ++j)
+    {
+        cow_write(&cow, '/');
+        if (_posix_normalize_impl(&cow, &state, fspaths[j]) != 0)
+        {
+            goto error;
+        }
+    }
+
+    if (cow.write_index == 0)
+    {
+        cow_write(&cow, '.');
+    }
+
+    joined = cow_consume(&cow);
 
 error:
-        // todo decref fspaths
-        free(fspaths);
-        return joined;
+    for (Py_ssize_t j = i; j < nargs; ++j)
+    {
+        Py_XDECREF(fspaths[j]);
+    }
+    free(fspaths);
+    return joined;
+}
+
+PyUnicodeObject *
+_posix_join(PyObject **args, Py_ssize_t nargs)
+{
+    switch (nargs)
+    {
+    case 0:
+        return _posix_join_zero();
+    case 1:
+        return _posix_join_one(args[0]);
+    default:
+        return _posix_join_many(args, nargs);
     }
 }
 
